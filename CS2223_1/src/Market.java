d1c65728baafc974c16a3a88660feaf8ac82280e
Alexandre Pauwels <ajpauwels@wpi.edu>
2014-11-08 18:48:13 -0500

    Adding in input processing using StdIn

--- CS2223_1/src/Market.java
+++ CS2223_1/src/Market.java
@@ -4,17 +4,31 @@ public class Market {
 	private PriorityQueue<Order> buyPQ;
 	private PriorityQueue<Order> sellPQ;
 	
-	public Market() {
+	public Market(String[] inputs) {
 		buyPQ = new PriorityQueue<Order>();
 		sellPQ = new PriorityQueue<Order>();
+		for (String input : inputs) {
+			String[] orderArray = input.split("\\s+");
+			Order order = new Order(orderArray[0], Integer.parseInt(orderArray[1]), Integer.parseInt(orderArray[2]));
+			
+			if (order.isBuy()) {
+				buyPQ.add(order);
+			} else {
+				sellPQ.add(order);
+			}
+		}
 	}
 	
-	public void Run(String[] input) {
+	public void run(String[] input) {
 		
 	}
 	
 	private boolean checkEquilibrium() {
 		if (buyPQ == null || sellPQ == null) return false;
 		return false;
+	}
+	
+	public void printTransactions() {
+		System.out.println("Nothing for now");
 	}
 }
--- CS2223_1/src/Trading.java
+++ CS2223_1/src/Trading.java
@@ -1,26 +1,15 @@
 
 public class Trading {
 	
+	/*
+	 * The main function simply takes in the buy/sell orders, feeds them to Market,
+	 * and then prints the transactions which occurred.
+	 * @param args
+	 */
 	public void main(String[] args) {
 		String[] inputs = StdIn.readAllLines();
-		for (String input : inputs) {
-			createOrder(input);
-		}
-	}
-	
-	private Order createOrder(String input) {
-		boolean isBuy;
-		int price;
-		int quantity;
-		String[] split = input.split(" ");
-		isBuy = (split[0].equals("buy") ? true : false);
-		price = Integer.parseInt(split[1]);
-		quantity = Integer.parseInt(split[2]);
-		return (new Order(isBuy, price, quantity));
-	}
-	
-	private boolean checkEquilibrium() {
-		return false;
+		Market market = new Market(inputs);
+		market.printTransactions();
 	}
 }
 
