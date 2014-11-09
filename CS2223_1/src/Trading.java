70c676ee45d64ec5ef0eb6a07681aeec023cabea
eacoeytaux <eacoeytaux@gmail.com>
2014-11-08 19:17:46 -0500

    Done!

--- CS2223_1/src/Trading.java
+++ CS2223_1/src/Trading.java
@@ -1,15 +1,9 @@
 
 public class Trading {
-	
-	/*
-	 * The main function simply takes in the buy/sell orders, feeds them to Market,
-	 * and then prints the transactions which occurred.
-	 * @param args
-	 */
-	public void main(String[] args) {
+
+	public static void main(String[] args) {
 		String[] inputs = StdIn.readAllLines();
 		Market market = new Market(inputs);
 		market.printTransactions();
 	}
 }
-
