604e823749ea2ca6c931c39fc82fe7c3aae101ba
Alexandre Pauwels <ajpauwels@wpi.edu>
2014-11-13 00:28:53 -0500

    Committing

--- CS2223_1/src/Trading.java
+++ CS2223_1/src/Trading.java
@@ -1,9 +1,10 @@
-
 public class Trading {
 
 	public static void main(String[] args) {
 		String[] inputs = StdIn.readAllLines();
-		Market market = new Market(inputs);
-		market.printTransactions();
+//		Market market = new Market(inputs);
+//		market.printTransactions();
+//		System.out.println("DONE");
+		System.out.println(inputs);
 	}
 }
